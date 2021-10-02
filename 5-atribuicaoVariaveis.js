b5f1be54e0cf52413781416aff5e630b098a14b8
HalyneSalgueiro <halynesalgueiroalves@hotmail.com>
2021-10-01 23:25:16 -0300

    Update 5-atribuicaoVariaveis.js

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,25 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 38;
+const nome = "Halyne";
+const sobrenome = "Salgueiro";
+
+//para incluir um espaço entre o nome e sobrenome console.log(nome +" " +sobrenome);
+//para incluir um espaço entre o nome e sobrenome console.log(nome, sobrenome);
+
+console.log(nome, sobrenome);
+//maneira usada em JS
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+let contador = 0
+contador = contador +1
+
+nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
+// variável melhor const do que let
+
+
+
+
+
+
