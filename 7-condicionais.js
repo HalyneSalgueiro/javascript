6766fb0d33705ac588e6125b6edabc181a4628c4
HalyneSalgueiro <halynesalgueiroalves@hotmail.com>
2021-10-03 10:34:20 -0300

    Create 7-condicionais.js

---
+++ 7-condicionais.js
@@ -0,0 +1,15 @@
+console.log(`Trabalhando com condicionais`);
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+);
+const idadeComprador = 15;
+console.log("Destinos possíveis:");
+//console.log(salvador, saoPaulo, rioDeJaneiro)
+console.log(listaDeDestinos);
+
+listaDeDestinos.splice(1,1); //removendo
+
+console.log(listaDeDestinos);
+
