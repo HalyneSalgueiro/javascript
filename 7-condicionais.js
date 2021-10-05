7cc6c1bd67f01496918c61f46ca9e237e64dd327
HalyneSalgueiro <halynesalgueiroalves@hotmail.com>
2021-10-05 14:08:01 -0300

    7-condicionais.js

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -4,12 +4,34 @@ const listaDeDestinos = new Array(
     `São Paulo`,
     `Rio de Janeiro`,
 );
-const idadeComprador = 15;
+
+const idadeComprador = 17;
+const estaAcompanhada = true //verdadeiro
 console.log("Destinos possíveis:");
-//console.log(salvador, saoPaulo, rioDeJaneiro)
 console.log(listaDeDestinos);
 
-listaDeDestinos.splice(1,1); //removendo
+if(idadeComprador >= 18){
+    console.log("Comprador maior de idade");
+    listaDeDestinos.splice(1,1); //removendo item    
+    
+}else{
+    //A pessoa é menor de idade
+    if(estaAcompanhada){
+        console.log("Comprador está acompanhado");
+        listaDeDestinos.splice(1,1); //removendo item    
+    }else{
+        console.log("Não é maior de Idade e não posso vender");
+        }
+}
 
 console.log(listaDeDestinos);
+
+// console.log(idadeComprador > 18);
+// console.log(idadeComprador < 18);
+// console.log(idadeComprador <= 18);
+// console.log(idadeComprador >= 18);
+// console.log(idadeComprador == 18);
+
+
+
 
