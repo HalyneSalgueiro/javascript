70e1c8247e5080139e09d6155440976d21330428
HalyneSalgueiro <halynesalgueiroalves@hotmail.com>
2021-10-05 18:29:43 -0300

    Classes

---
+++ index.js
@@ -0,0 +1,26 @@
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Samuel";
+cliente1.cpf = 11122233309;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233304;
+cliente2Agencia = 1001;
+cliente2Saldo = 0;
+
+const cliente3Nome = "Davi";
+const cliente3CPF = 88820348724;
+const cliente3Agencia = 1001;
+const cliente3Saldo = 0;
+
+console.log(cliente1);
