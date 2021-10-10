4d57c5fcb6fa5111f9ec56d1b742db34908c2046
HalyneSalgueiro <halynesalgueiroalves@hotmail.com>
2021-10-10 17:10:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Samuel";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233304;
-cliente2Agencia = 1001;
-cliente2Saldo = 0;
 
-const cliente3Nome = "Davi";
-const cliente3CPF = 88820348724;
-const cliente3Agencia = 1001;
-const cliente3Saldo = 0;
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
 
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(300);
+
+console.log(contaCorrenteRicardo.saldo);
 console.log(cliente1);
+console.log(cliente2)
